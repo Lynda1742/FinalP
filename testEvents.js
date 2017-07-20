d9f9e87dd2ac127c65c2f730f905ec2b2f48d9e5
Lynda <lynda1742@gmail.com>
2017-07-20 15:20:24 -0500

    thursday final upload'

---
+++ testEvents.js
@@ -0,0 +1,13 @@
+function addEvent() {
+  var eventname = document.getElementById('EvName');
+  var eventtime = document.getElementById('EvTime');
+  var eventloc = document.getElementById('EvLoc');
+  var table = document.getElementById('tabEv');
+  var row = table.insertRow(1);
+  var cell_evname = row.insertCell(0);
+  var cell_evtime = row.insertCell(1);
+  var cell_evloc = row.insertCell(2);
+  cell_evname.innerHTML = eventname.value;
+  cell_evtime.innerHTML = eventtime.value;
+  cell_evloc.innerHTML = eventloc.value;
+}
