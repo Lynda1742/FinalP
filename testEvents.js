4a9dbaf6ee9d3319c3bf6e7a3dae8a733173f75b
Lynda <lynda1742@gmail.com>
2017-07-21 11:52:38 -0500

    friday morning updates'

--- testEvents.js
+++ testEvents.js
@@ -7,7 +7,16 @@ function addEvent() {
   var cell_evname = row.insertCell(0);
   var cell_evtime = row.insertCell(1);
   var cell_evloc = row.insertCell(2);
+  var cell_evcause = row.insertCell(3);
   cell_evname.innerHTML = eventname.value;
   cell_evtime.innerHTML = eventtime.value;
   cell_evloc.innerHTML = eventloc.value;
+  for (c = 0; c < 13; c++) {
+    var pickchoice = document.getElementById(c).checked;
+    console.log(pickchoice)
+    if (pickchoice == true) {
+      console.log(document.getElementById(00).value);
+      cell_evcause.innerHTML += document.getElementById(c).value + ", ";
+    }
+  }
 }
